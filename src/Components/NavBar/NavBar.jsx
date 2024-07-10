3d81999bff9a949ec1766356ffa60dc875c2c713
REDAEZZAMRANI <ezzamranireda@gmail.com>
2024-07-10 13:07:53 +0100

    intialisation the project

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar(props) {
     console.log(props.position)
     return (
 
-        <nav className="nav-bar" style={{position:props.position}}>
+        <nav className="nav-bar navbar" style={{position:props.position}}>
             <div className="logo">
                 <img src={Logo} alt="" />
             </div>
@@ -18,6 +18,24 @@ export default function NavBar(props) {
                 <NavLink to='/contact'>Contact</NavLink>
                 
             </div>
+        
+         <div class="navbar-container container">
+             <input type="checkbox" name="" id=""/>
+             <div class="hamburger-lines">
+                 <span class="line line1"></span>
+                 <span class="line line2"></span>
+                 <span class="line line3"></span>
+             </div>
+             <ul class="menu-items">
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">About</a></li>
+                 <li><a href="#">Category</a></li>
+                 <li><a href="#">Menu</a></li>
+                 <li><a href="#">Testimonial</a></li>
+                 <li><a href="#">Contact</a></li>
+             </ul>
+             
+         </div>
         </nav>
 
     )
